5c96593c0069bd15c3f0f8c22e89feddf1525d10
Kevin Ochieng <kevincliffo@gmail.com>
2021-08-29 22:52:35 +0300

    Added new fields for appointment

--- application/views/view_add_appointment.php
+++ application/views/view_add_appointment.php
@@ -66,7 +66,68 @@
                                             <label for="lastName">Last Name</label>
                                             <input type="text" class="form-control" id="lastName" name="lastName" placeholder="Last Name">
                                         </div>
-                                    </div>                                    
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <div class="form-group">
+                                            <label for="mobileNo">Mobile No</label>
+                                            <input type="number" class="form-control" id="mobileNo" name="mobileNo" placeholder="Mobile No">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="idNumber">ID Number</label>
+                                            <input type="number" class="form-control" id="idNumber" name="idNumber" placeholder="ID Number">
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <div class="form-group">
+                                            <label for="gender">Gender</label>
+                                            <select class="form-control" id="gender" name="gender">
+                                                <option selected="selected" disabled>Select Gender</option>
+                                                <option value="Female">Female</option>
+                                                <option value="Male">Male</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Date of Birth:</label>
+                                            <div class="input-group date" id="startDate" data-target-input="nearest">
+                                                <input type="text" class="form-control datetimepicker-input" name="dob" data-target="#startDate"/>
+                                                <div class="input-group-append" data-target="#startDate" data-toggle="datetimepicker">
+                                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <div class="form-group">
+                                            <label for="patientType">Patient Type</label>
+                                            <select class="form-control" id="patientType" name="patientType">
+                                                <option selected="selected" disabled>Select Patient Type</option>
+                                                <option value="Baby">Baby</option>
+                                                <option value="Child">Child</option>
+                                                <option value="Adult">Adult</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="patientImage">Patient Image</label>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input" id="patientImage" name="patientImage">
+                                                    <label class="custom-file-label" for="exampleInputFile">Select Image</label>
+                                                </div>
+                                            </div>                                    
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="address">Address</label>
+                                        <textarea class="form-control" id="address" name="address" placeholder="Address"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="location">Location</label>
+                                        <textarea class="form-control" id="location" name="location" placeholder="Location"></textarea>
+                                    </div>  
+                                    <div class="form-group">
+                                        <label for="underlyingCondition">Underlying Condition</label>
+                                        <textarea class="form-control" id="underlyingCondition" name="underlyingCondition" placeholder="Underlying Condition"></textarea>
+                                    </div>                                                                                                                                              
                                     <div class="form-group">
                                         <label for="symptoms">Symptoms</label>
                                         <textarea class="form-control" id="symptoms" name="symptoms" placeholder="Symptoms"></textarea>
